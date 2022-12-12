2a6934c24e4ac5c00648777765f6748aae1bcc3d
Djara2 <jaradavid@protonmail.com>
2022-12-11 18:56:23 -0600

    added function to calculate lateral distance of 2 notes on the fretboard

--- tab_generator.py
+++ tab_generator.py
@@ -1,4 +1,27 @@
 WHOLE_STEPS = ["A", "B", "C", "D", "E", "F", "G"]
+
+TUNINGS = {
+        # NOTE: 7 and 8 string guitar tunings can be created by simply appending extended range to existing 6 string tuning; for example, B standard (7 string) = ["B"] + TUNINGS["E standard"] 
+
+        # 6 String standard tunings
+        "E standard": ["E", "A", "D", "G", "B", "E"],
+        # Eb standard is the same as D# standard
+        "Eb standard" : ["Eb", "Ab", "Db", "Gb", "Bb", "Eb"], 
+        "D standard" : ["D", "G", "C", "F", "A", "D"],
+        # C# standard is the same as Db standard
+        "C# standard": ["C#", "F#", "B", "E", "G#", "C#"] 
+        "C standard" : ["C", "F", "A#", "D#", "G", "C"],
+        "B standard" : ["B", "E", "A", "D", "G", "B"],
+        "A standard" : ["A", "D", "G", "C", "E", "A"],
+        
+        # 6 string drop tunings
+        "Drop D": ["D", "A", "D", "G", "B", "E"],
+        "Drop C#": ["C#", "G#", "C#", "F#", "A#", "D#"],
+        "Drop C": ["C", "G", "C", "F", "A", "D"],
+        "Drop B": ["B", "F#", "B", "E", "G#", "C#"],
+        "Drop A": ["A", "E", "A", "D", "G", "B"]
+        }
+
 class Note:
     def __init__(self, note: str, octave: int):
         self.note = note
@@ -36,6 +59,7 @@ class FretboardPosition:
         self.string = string
         self.fret = fret
 
+   
 TAB_MAP = {
         # 6th string
         # Normally, I prefer to play laterally, rather than jumping down to the next string.
@@ -46,11 +70,11 @@ TAB_MAP = {
         Note("G", 0) : FretboardPosition(6, 3),
         Note("Ab", 0) : FretboardPosition(6, 4),
         Note("A", 0) : FretboardPosition(6, 5),
-        Note("E", 0) : FretboardPosition(6, 6),
-        Note("E", 0) : FretboardPosition(6, 7),
-        Note("E", 0) : FretboardPosition(6, 8),
-        Note("E", 0) : FretboardPosition(6, 9),
-        Note("E", 0) : FretboardPosition(6, 10),
+        Note("A#", 0) : FretboardPosition(6, 6),
+        Note("B", 0) : FretboardPosition(6, 7),
+        Note("C", 0) : FretboardPosition(6, 8),
+        Note("C#", 0) : FretboardPosition(6, 9),
+        Note("D", 0) : FretboardPosition(6, 10),
         
         # 5th string
         Note("D", 0) : FretboardPosition("A", 5),
@@ -61,29 +85,13 @@ TAB_MAP = {
         # STILL ADDING MORE TO THIS
         }
 
-
-TUNINGS = {
-        # NOTE: 7 and 8 string guitar tunings can be created by simply appending extended range to existing 6 string tuning; for example, B standard (7 string) = ["B"] + TUNINGS["E standard"] 
-
-        # 6 String standard tunings
-        "E standard": ["E", "A", "D", "G", "B", "E"],
-        # Eb standard is the same as D# standard
-        "Eb standard" : ["Eb", "Ab", "Db", "Gb", "Bb", "Eb"], 
-        "D standard" : ["D", "G", "C", "F", "A", "D"],
-        # C# standard is the same as Db standard
-        "C# standard": ["C#", "F#", "B", "E", "G#", "C#"] 
-        "C standard" : ["C", "F", "A#", "D#", "G", "C"],
-        "B standard" : ["B", "E", "A", "D", "G", "B"],
-        "A standard" : ["A", "D", "G", "C", "E", "A"],
-        
-        # 6 string drop tunings
-        "Drop D": ["D", "A", "D", "G", "B", "E"],
-        "Drop C#": ["C#", "G#", "C#", "F#", "A#", "D#"],
-        "Drop C": ["C", "G", "C", "F", "A", "D"],
-        "Drop B": ["B", "F#", "B", "E", "G#", "C#"],
-        "Drop A": ["A", "E", "A", "D", "G", "B"]
-        }
-
+ def lateral_fretboard_distance(note1, note2):
+    """returns the lateral distance (how many frets away) this note is from another note on the fretboard. Used to determine whether or not the note should be on the same string, or a different string. E.g. it is extremely difficult to go from the 3rd fret to the 9th fret on the E string, so it would be better to play the 3rd fret on the E string, then the 4th fret on the A string."""
+    if TAB_MAP[note1].string == TAB_MAP[note2].string:
+        highest_fret = max(TAB_MAP[note1].fret, TAB_MAP[note2].fret)
+        lowest_fret = min(TAB_MAP[note1].fret, TAB_MAP[note2].fret)
+        return highest_fret - lowest_fret
+    
 def determine_tuning(notes: list):
     """
     Determines the tuning the guitar should be in, based on the lowest played note.
