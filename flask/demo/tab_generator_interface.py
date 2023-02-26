861d52457a225796f98707fb31b02815c95e8729
NathanGranade <ngranade987@gmail.com>
2023-02-26 14:38:24 -0600

    fixed bug in TGI - Nathan

--- flask/demo/tab_generator_interface.py
+++ flask/demo/tab_generator_interface.py
@@ -57,35 +57,35 @@ def get_batch(notes):
 DEBUG = False
 
 if argc > 1:
-    for arg in argv[1:]:
-        # read the contents of the file passed to the program
-        # this file should contain note representations
-        notes = read_notes(arg)
-        if DEBUG:
-            print(notes)
-        
-        # the format of notes in the notes list should be for
-        # the transcriber, which is not how notes are represented
-        # by the tab generator. So convert the notes to the
-        # internal representation of the generator, using the
-        # parse_transcriber_note method
-        generator_compatible_notes = []
-        for note in notes:
-            generator_compatible_notes.append(tab_generator.parse_transcriber_note(note))
-        if DEBUG:
-            for note in generator_compatible_notes:
-                print(str(note))
-        # generate several tab_dictionary instances containing at most
-        # 15 notes so as to make the overall tab broken up into small
-        # and easily digestable segments, like how a human would write
-        # a tab
-        output_file_name = generate_output_filename(arg)
-        print(output_file_name)
-        output_file = open(f"{output_file_name}-tab.txt", "a")
-        while len(generator_compatible_notes) > 0:
-            segment, generator_compatible_notes = get_batch(generator_compatible_notes)
-            tab_dictionary = tab_generator.generate_tab_dictionary(segment)
-            tab = tab_generator.generate_tab(tab_dictionary)
-            #print(f"{tab}\n")
-            output_file.write(f"{tab}\n")
-        output_file.close()
+   with open(f"RawNotes/RawNotes-tab.txt", "w") as f:
+        for arg in argv[1:]:
+            # read the contents of the file passed to the program
+            # this file should contain note representations
+            notes = read_notes(arg)
+            if DEBUG:
+                print(notes)
+            
+            # the format of notes in the notes list should be for
+            # the transcriber, which is not how notes are represented
+            # by the tab generator. So convert the notes to the
+            # internal representation of the generator, using the
+            # parse_transcriber_note method
+            generator_compatible_notes = []
+            for note in notes:
+                generator_compatible_notes.append(tab_generator.parse_transcriber_note(note))
+            if DEBUG:
+                for note in generator_compatible_notes:
+                    print(str(note))
+            # generate several tab_dictionary instances containing at most
+            # 15 notes so as to make the overall tab broken up into small
+            # and easily digestable segments, like how a human would write
+            # a tab
+            
+            
+            while len(generator_compatible_notes) > 0:
+                segment, generator_compatible_notes = get_batch(generator_compatible_notes)
+                tab_dictionary = tab_generator.generate_tab_dictionary(segment)
+                tab = tab_generator.generate_tab(tab_dictionary)
+                #print(f"{tab}\n")
+                f.write(f"{tab}\n")
+            f.close()
