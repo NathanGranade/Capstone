f6a06d44e9ef18b049460b1350c789645670bc95
AaronEarp <115114961+AaronEarp@users.noreply.github.com>
2023-04-10 02:16:22 -0500

    User can now login after registering, log out is not implemented yet, also no login functionalities

--- clabtest/react/server/server.py
+++ clabtest/react/server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from views import views
 from flask import Flask, flash, render_template, request, redirect, url_for, session
+from flask import flash
 from flask_dropzone import Dropzone
 from flask_mysqldb import MySQL
 from werkzeug.utils import secure_filename
@@ -155,8 +156,8 @@ def search():
             cursor.close()
             return render_template('display.html', var = songID)
 
-@app.route('/login', methods = ['POST', 'GET'])
-def login():
+@app.route('/register', methods = ['POST', 'GET'])
+def register():
     if request.method == 'GET':
         return ""
      
@@ -166,23 +167,38 @@ def login():
         password = request.form['password']
         validPW = validatepw(password)
         if validPW == 0:
-            return "Please input a valid password. A valid password uses a number, a special character, a capital letter, and has a length between 8 and 20 characters."
+            return ("Please input a valid password. A valid password uses a number, a special character, a capital letter, and has a length between 8 and 20 characters.")
         validEmail = validateEmail(email)
         if validEmail == 0:
-            return "Please input a valid email address."
+            return ("Please input a valid email address.")
         validUser = validateUser(username)
         if validUser == 0:
-            return "Please input a valid username. A valid username is at least 8 characters and contains no special characters."
+            return ("Please input a valid username. A valid username is at least 8 characters and contains no special characters.")
         idUser = random.randrange(100)
         cursor = mysql.connection.cursor()
         cursor.execute(''' INSERT INTO Users (Email,Username,Password,idUser) VALUES(%s,%s,%s,%s)''',(email,username,password,idUser))
         mysql.connection.commit()
         cursor.close()
     if email and username and password:
-        return "USER REGISTERED"
+        return ("User registered")
     else:
-        return "Please fill out all forums."
+        return ("Please fill out all forms.")
 
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        loginusername = request.form['username']
+        loginpassword = request.form['password']
+        cursor = mysql.connection.cursor()
+        ##sql ='SELECT idUser from Users WHERE Username = {0}'.format(loginusername)
+        cursor.execute("select * from Users where (Username = '%s') AND (Password = '%s')" % (loginusername, loginpassword))
+        if cursor.rowcount == 0:
+            return "User does not exist, try again, or go sign up!" 
+        else:  
+            global loggedin == true
+            return "Logged in!"
+    if request.method == 'GET':
+        return ""
 
 if __name__ == '__main__':
     app.run(debug = True, port = 8000)
